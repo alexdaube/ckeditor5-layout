e3735cd65797a4408a0290d8ee3f164d2494ad99
alexdaube <alexdaube@gmail.com>
2021-06-01 12:26:07 -0400

    Upgrading dependencies and imports

--- src/layout.js
+++ src/layout.js
@@ -1,4 +1,4 @@
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin'
+import { Plugin } from 'ckeditor5/src/core';
 import ColumnLayout from './columns/columns'
 import LayoutUI from "./layoutui";
 
